e0f12959c4515c8f8f63688a6a96fe1c9c468437
Hend Mohammed <hendmohammed10420@example.com>
2023-04-10 22:52:47 +0200

    Edit dynamic reting

--- src/app/components/userbooks/userbooks.component.ts
+++ src/app/components/userbooks/userbooks.component.ts
@@ -1,136 +1,93 @@
-import { Component, EventEmitter, ViewChild } from '@angular/core';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatTabChangeEvent, MatTabsModule } from '@angular/material/tabs';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatPaginator, MatPaginatorIntl } from '@angular/material/paginator';
-import { MatCardModule } from '@angular/material/card';
- 
-interface Book{
-  cover: string;
-  name: string;
-  author: string;
-  avgRate: number;
-  rating: number;
-  shelve: string;
+import { Component, EventEmitter } from '@angular/core';
+import { CBAService } from 'src/app/services/cba.service';
 
-}
 @Component({
   selector: 'app-userbooks',
   templateUrl: './userbooks.component.html',
-  
   styleUrls: ['./userbooks.component.scss']
 })
 
 export class UserbooksComponent {
-  
-// ----------------------------------------------------------------------//
-                  //                  عاوز تغير هنا براحتك بس حافظ ع المسميات دى عشان مفيش حاااجة تضرب لاقتلك  ^_^
-  filteredBooks: Book[] = [];
-  // stars = [1, 2, 3, 4, 5];
-  books: Book[] = [
-    {
-      cover: 'book 1 cover image url',
-      name: 'Book 1',
-      author: 'Author 1',
-      avgRate: 4.5,
-      rating: 4.7,
-      shelve: 'reading'
-    },
-    {
-      cover: 'book 2 cover image url',
-      name: 'Book 2',
-      author: 'Author 2',
-      avgRate: 4.2,
-      rating: 3.1,
-      shelve: 'want-to-read'
-    },
-    {
-      cover: 'book 3 cover image url',
-      name: 'Book 3',
-      author: 'Author 3',
-      avgRate: 3.5,
-      rating: 3.8,
-      shelve: 'read'
-    },
-  ];
-  tabs = [
-    {
-      label: 'All',
-      value: ''
-    },
-    {
-      label: 'Read',
-      value: 'read'
-    },
-    {
-      label: 'Currently Reading',
-      value: 'reading'
-    },
-    {
-      label: 'Want to read',
-      value: 'want-to-read'
-    }
-  ];
-  //  --------------------------------------------------------------//
+  currentPage:number = 1;
+  limit:number = 5;
+  shelf:string='';
+  books:any = []
+  option:any = []
 
 
-
-
-
-
-
-
-  // starRating = 0; 
-
-// ملكش دعوة بال functions  دى احسنلك لقد اعذر من انذر
-
-  
-@ViewChild(MatPaginator) paginator!: MatPaginator;
-
-  constructor(private matPaginatorIntl: MatPaginatorIntl) {
-    this.matPaginatorIntl.itemsPerPageLabel = 'Books per page:';
-    this.selectedTabIndex = this.tabs.findIndex(tab => tab.label === 'All');
+  constructor(private _CBAService: CBAService) {
+    this.getBooks()
   }
 
-
-
-  selectedTabIndex: number;
+  getBooks(){
+    this._CBAService.getCBA('user/book', this.currentPage, this.limit,`&shelf=${this.shelf}`).subscribe((res) => {      
+      if (res.status == 200) {
+        console.log(res);
+        
+        this.option = res.body.data.option;
+        this.books = res.body.data.docs;
+        console.log(this.books);
+      }
+    });
+  }
 
   onTabChange(event: any) {
-    this.selectedTabIndex = event.index;
+    console.log(event.index);
+    if(event.index===0){
+      this.shelf='';
+      this.getBooks();
+      return ;
+    }
 
-    if (this.selectedTabIndex === 1) {
-      this.filteredBooks = this.books;
-    this.paginator.firstPage();
+    if(event.index===1){
+      this.shelf='read';
+      this.getBooks();
+      return ;
+    }
 
-    } else {
-      this.filteredBooks = this.books.filter(book => book.shelve === this.tabs[this.selectedTabIndex].value);
-      this.paginator.firstPage();
+    if(event.index===2){
+      this.shelf='currently reading';
+      this.getBooks();
+      return ;
+    }
 
+    if(event.index===3){
+      this.shelf='want to read';
+      this.getBooks();
+      return ;
     }
   }
 
-  onPageChange(event: any) {
-    const startIndex = event.pageIndex * event.pageSize;
-    const endIndex = startIndex + event.pageSize;
-    this.filteredBooks = this.filteredBooks.slice(startIndex, endIndex);
+  get math() {
+    return Math;
   }
-  rating: number = 0;
-  large: boolean = false;
-  extraLarge: boolean = false;
-  ratingChange = new EventEmitter<number>();
-  stars: { filled: boolean, hover: boolean }[] = [];
+  floor(value: number) {
+    return this.math.floor(value);
+  }
 
-  ngOnInit(): void {
-    this.books;
-    this.stars = Array(5).fill(null).map(() => ({ filled: false, hover: false }));
-    const roundedRating = Math.round(this.rating);
-    for (let i = 0; i < roundedRating; i++) {
-      this.stars[i].filled = true;
+  ceil(value: number) {
+    return this.math.ceil(value);
+  }
+
+
+
+  nextPage(){
+    if(this.option.hasNextPage) {
+      this.currentPage=this.option.nextPage;
+      this.getBooks()
     }
   }
+  prevPage(){
+    if(this.option.hasPrevPage) {      
+      this.currentPage=this.option.prevPage;
+      this.getBooks();
+    }
+  }
+  //rating: number = 0;
+  //ratingChange = new EventEmitter<number>();
+  stars: { filled: boolean, hover: boolean }[] = Array(5).fill(null).map(() => ({ filled: false, hover: false }));
+
+
 
   onStarHover(star: any) {
     star.hover = true;
@@ -140,13 +97,28 @@ export class UserbooksComponent {
     star.hover = false;
   }
 
-  onStarClick(star: any) {
-    const rating = this.stars.indexOf(star) + 1;
-    for (let i = 0; i < this.stars.length; i++) {
-      this.stars[i].filled = i < rating;
+  onStarClick(star: any,bookId:string,bookShelf:String) {
+    const rating=this.stars.indexOf(star) + 1
+    console.log(rating,bookId,bookShelf);
+    this.updateShelf(rating,bookId,bookShelf)
+  }
+
+  Change(target:any,bookId:string,rating:number){
+    this.updateShelf(rating,bookId,target.value)
+  }
+
+
+  updateShelf(rating: number,bookId:string,bookShelf:String){
+    const obj:object={
+      "shelf": bookShelf,
+      "rating": rating
     }
-    this.rating = rating;
-    this.ratingChange.emit(this.rating);
-  }
+    this._CBAService.patchCBA('user/book',bookId,obj).subscribe((res) => {
+      if(res.status==200)
+        console.log('Shelf Updated');
+        this.getBooks()
+    })
+  }
+
 
 }
